6c51435acded12bd802bbbb830506b64659c13ad
User <berkayolce@gmail.com>
2020-09-03 18:49:31 +0300

    SweetAlert ile Confirm kullanilarak Kayıt silme işlemi gerçeklestirildi

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something went wrong!',
-        footer: '<a href>Why do I have this issue?</a>'
-      })
+$(document).ready(function () {
+
+    $(".remove-btn").click(function (e) {
+
+        var $data_url = $(this).data("url");
+        Swal.fire({
+            icon: 'warning',
+            title: 'Emin misiniz ?',
+            text: 'Bu işlemi geri alamayacaksınız !',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Sil!',
+            cancelButtonText: 'İptal',
+        }).then(function(result){
+            if(result.value){
+                window.location.href = $data_url;
+            }
+        });
+    })
 })
