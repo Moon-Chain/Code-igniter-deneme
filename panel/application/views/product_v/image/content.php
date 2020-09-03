44cca1b73892739d4a96485bdf655f2f7d60c60f
User <berkayolce@gmail.com>
2020-09-03 23:27:32 +0300

    upload edilen resimlerin getirilmesi

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -21,30 +21,45 @@
     <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
-                <table class="table table-bordered table-striped table-hover pictures_list">
-                    <thead>
-                        <th>#id</th>
-                        <th>Görsel</th>
-                        <th>Resim Adı</th>
-                        <th>Durumu</th>
-                        <th>İşlem</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="w100 text-center">#1</td>
-                            <td class="w100 text-center"><img width="50" src="https://www.edigitalagency.com.au/wp-content/uploads/square-payments-logo-png-transparent-background-vertical.png" class="img-responsive"></td>
-                            <td>deneme-urunu.jpg</td>
-                            <td class="w100 text-center">
-                                <input data-url="<?php echo base_url("product/isActiveSetter/"); ?>" class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo (true) ? "checked" : ""; ?> />
-                            </td>
-                            <td class="w100 text-center">
-                                <button data-url="<?php echo base_url("product/delete/"); ?>" type="button" class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
-                                    <i class="fa fa-trash"></i> Sil
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+
+                <?php if (empty($item_images)) { ?>
+
+                    <div class="alert alert-info text-center">
+                        <p>Burada herhangi bir resim bulunmamaktadır.</p>
+                    </div>
+
+                <?php } else { ?>
+
+                    <table class="table table-bordered table-striped table-hover pictures_list">
+                        <thead>
+                            <th>#id</th>
+                            <th>Görsel</th>
+                            <th>Resim Adı</th>
+                            <th>Durumu</th>
+                            <th>İşlem</th>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($item_images as $image) { ?>
+                                <tr>
+                                    <td class="w100 text-center">#<?php echo $image->id; ?></td>
+                                    <td class="w100 text-center">
+                                        <img width="50" src="<?php echo base_url("uploads/{$viewFolder}/$image->img_url"); ?>" alt="<?php echo $image->img_url; ?>" class="img-responsive">
+                                    </td>
+                                    <td><?php echo $image->img_url; ?></td>
+                                    <td class="w100 text-center">
+                                        <input data-url="<?php echo base_url("product/isActiveSetter/"); ?>" class="isActive" type="checkbox" data-switchery data-color="#10c469" <?php echo ($image->id) ? "checked" : ""; ?> />
+                                    </td>
+                                    <td class="w100 text-center">
+                                        <button data-url="<?php echo base_url("product/delete/"); ?>" type="button" class="btn btn-sm btn-danger btn-outline remove-btn btn-block">
+                                            <i class="fa fa-trash"></i> Sil
+                                        </button>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+
+                <?php } ?>
             </div>
         </div>
     </div>
