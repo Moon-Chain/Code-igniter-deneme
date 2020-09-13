c18c7cabc82ee53f1e23e3deef3f5742ce6dd9bf
User <berkayolce@gmail.com>
2020-09-13 19:48:41 +0300

    DB ayarlamaları yapıldı, Galeri modülü için controller, model oluşturuldu ve dosya oluşturma işlemi yapıldı

---
+++ panel/application/views/galleries_v/image/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <?php $this->load->view("includes/head");?>
+	<?php $this->load->view("{$viewFolder}/{$subViewFolder}/page_style");?>
+  
+</head>
+	
+<body class="menubar-left menubar-unfold menubar-light theme-primary">
+<!--============= start main area -->
+<?php $this->load->view("includes/navbar"); ?>
+
+<?php $this->load->view("includes/aside"); ?>
+
+<?php $this->load->view("includes/navbar-search"); ?>
+
+<!-- APP MAIN ==========-->
+<main id="app-main" class="app-main">
+  <div class="wrap">
+	<section class="app-content">
+		<?php $this->load->view("{$viewFolder}/{$subViewFolder}/content"); ?>
+	</section>
+</div>
+
+  <!-- APP FOOTER -->
+  <?php $this->load->view("includes/footer"); ?>
+  <!-- /#app-footer -->
+</main>
+<!--========== END app main -->
+
+    <?php $this->load->view("includes/include_script");?>
+</body>
+</html>
--- panel/application/views/includes/aside.php
+++ panel/application/views/includes/aside.php
@@ -3,7 +3,7 @@
     <div class="media">
       <div class="media-left">
         <div class="avatar avatar-md avatar-circle">
-          <a href="javascript:void(0)"><img class="img-responsive" src="<?php echo base_url("assets"); ?>/assets/images/221.jpg" alt="avatar"/></a>
+          <a href="javascript:void(0)"><img class="img-responsive" src="<?php echo base_url("assets"); ?>/assets/images/221.jpg" alt="avatar" /></a>
         </div><!-- .avatar -->
       </div>
       <div class="media-body">
@@ -54,7 +54,7 @@
       <ul class="app-menu">
 
 
-      <li>
+        <li>
           <a href="javascript:void(0)">
             <i class="menu-icon zmdi zmdi-view-dashboard zmdi-hc-lg"></i>
             <span class="menu-text">Dashboard</span>
@@ -71,10 +71,11 @@
         <li class="has-submenu">
           <a href="javascript:void(0)" class="submenu-toggle">
             <i class="menu-icon zmdi zmdi-apps zmdi-hc-lg"></i>
-            <span class="menu-text">Galeriler</span>
+            <span class="menu-text">Galeri İşlemleri</span>
             <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
           </a>
           <ul class="submenu">
+            <li><a href="<?php echo base_url("galleries");?>"><span class="menu-text">Galeriler</span></a></li>
             <li><a href="#"><span class="menu-text">Resim Galerisi</span></a></li>
             <li><a href="#"><span class="menu-text">Video Galerisi</span></a></li>
             <li><a href="#"><span class="menu-text">Dosya Galerisi</span></a></li>
@@ -111,35 +112,35 @@
 
         <li>
           <a href="<?php echo base_url("references"); ?>">
-          <i class="menu-icon zmdi zmdi-check zmdi-hc-lg"></i>
+            <i class="menu-icon zmdi zmdi-check zmdi-hc-lg"></i>
             <span class="menu-text">Referanslar</span>
           </a>
         </li>
-        
+
         <li>
           <a href="javascript:void(0)">
-          <i class="menu-icon fa fa-user-secret"></i>
+            <i class="menu-icon fa fa-user-secret"></i>
             <span class="menu-text">Kullanıcılar</span>
           </a>
         </li>
 
         <li>
           <a href="javascript:void(0)">
-          <i class="menu-icon fa fa-users"></i>
+            <i class="menu-icon fa fa-users"></i>
             <span class="menu-text">Aboneler</span>
           </a>
         </li>
 
         <li>
           <a href="<?php echo base_url("brands"); ?>">
-          <i class="menu-icon zmdi zmdi-puzzle-piece zmdi-hc-lg"></i>
+            <i class="menu-icon zmdi zmdi-puzzle-piece zmdi-hc-lg"></i>
             <span class="menu-text">Markalar</span>
           </a>
         </li>
 
         <li>
           <a href="javascript:void(0)">
-          <i class="menu-icon zmdi zmdi-lamp zmdi-hc-lg"></i>
+            <i class="menu-icon zmdi zmdi-lamp zmdi-hc-lg"></i>
             <span class="menu-text">Popup Hizmeti</span>
           </a>
         </li>
